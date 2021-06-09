ad53a43eb7c0bd52ad2028f3dc645cc47d1cbccb
vdo162 <vdo162@gmail.com>
2021-06-09 07:10:19 +0300

    changed a render method in Board to cycles

--- src/index.js
+++ src/index.js
@@ -16,31 +16,25 @@ const Square = (props) => {
 class Board extends React.Component {
   renderSquare(i) {
     return (
-	  <Square 
+	  <Square key={i}
 		value={this.props.squares[i]} 
 		onClick={() => this.props.onClick(i)} 
 	  />
 	);
   }
 
-  render() { 
+  render() {
+	let square = [];
+	let squares = [];
+	for (let j = 0; j < 3; j++) {
+		for (let i = 0; i < 3; i++) {
+			square[i] = this.renderSquare(j*3+i);
+		}
+		squares[j] = <div>{square.slice()}</div>
+	}
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+		{squares}
       </div>
     );
   }
